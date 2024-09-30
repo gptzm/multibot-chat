e0f521fedf2cab6c28c888399a30e1be503deab3
zm <13182752+gptzm@user.noreply.gitee.com>
2024-09-30 20:08:17 +0800

    修复手动让观众发言时会报错的问题

--- custom_pages/utils/bot_display.py
+++ custom_pages/utils/bot_display.py
@@ -161,6 +161,7 @@ def display_group_chat_area(bot_manager, show_bots, histories):
             for i, bot in enumerate(disabled_bots):
                 col = cols[i % num_cols]
                 with col:
+                    group_user_prompt = chat_config.get('group_user_prompt')
                     if st.button(f"{bot.get('avatar', '🤖')} {bot['name']}\n\n{bot['engine']} {bot.get('model','')}", key=f"group_bot_{bot['id']}", help=bot.get('system_prompt','')[0:100], use_container_width=True):
                         response_content = get_response_from_bot_group(group_user_prompt, bot, histories)
                         bot_manager.add_message_to_group_history("assistant", response_content, bot=bot)
