6d864d04735a1b58887132880ebd41e1f7d5e0bd
zm <13182752+gptzm@user.noreply.gitee.com>
2024-09-30 20:12:14 +0800

    fix bug

--- custom_pages/utils/bot_display.py
+++ custom_pages/utils/bot_display.py
@@ -161,6 +161,7 @@ def display_group_chat_area(bot_manager, show_bots, histories):
             for i, bot in enumerate(disabled_bots):
                 col = cols[i % num_cols]
                 with col:
+                    chat_config = bot_manager.get_chat_config()
                     group_user_prompt = chat_config.get('group_user_prompt')
                     if st.button(f"{bot.get('avatar', '🤖')} {bot['name']}\n\n{bot['engine']} {bot.get('model','')}", key=f"group_bot_{bot['id']}", help=bot.get('system_prompt','')[0:100], use_container_width=True):
                         response_content = get_response_from_bot_group(group_user_prompt, bot, histories)
